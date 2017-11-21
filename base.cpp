50c5385fe21c792b10a3f5fe771ef2e222f480e9
Guilherme Tassinari <guitassinari@gmail.com>
2017-11-21 19:08:17 -0200

    canny edges

--- base.cpp
+++ base.cpp
@@ -18,18 +18,27 @@ int main(int argc, char** argv)
   if(!cap.open(camera))
   return 0;
   namedWindow("Config", 1);
+  namedWindow("Canny", 1);
   createTrackbar("TESTE", "Config", &kernelSize, 101, increaseKernelSize);
   for(;;)
   {
     Mat frame;
     Mat editedFrame;
+    Mat cannyEdge;
+    Mat gray;
+
+
+
     cap >> frame;
     if( frame.empty()){
       break;// end of video stream
     }
 
-    GaussianBlur(frame, editedFrame, Size(kernelSize, kernelSize), 0, 0);
+    cvtColor(frame, gray, CV_BGR2GRAY);
 
+    Canny(gray, cannyEdge, 50, 50, 3);
+    GaussianBlur(frame, editedFrame, Size(kernelSize, kernelSize), 0, 0);
+    imshow("Canny", cannyEdge);
     imshow("This is you, smile! :)", editedFrame);
     if(waitKey(1)==27){
       break;// stop capturing by pressing ESC
